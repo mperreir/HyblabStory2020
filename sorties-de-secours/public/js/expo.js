ebedce727015f26cdc73017c3ac2f86888a53b74
Unknown <alexis.aigueparse@gmail.com>
2020-01-22 17:08:43 +0100

    Basics
    - added expo slide
    - added dance slide

--- sorties-de-secours/public/js/expo.js
+++ sorties-de-secours/public/js/expo.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
 	// set variables for svg elements
 	var circle_size = 30;
 	var random_range = 5;
+	//var elements_pos = [];
+	//var space_between_elements = 10;
 
 	// tick make only 1/max_tick circle printable (and offers "scratch effet")
-	var tick = 0;
-	var max_tick = 5;
+	//var tick = 0;
+	//var max_tick = 5;
 
 	// init dragging
 	var isDragging = false;
@@ -20,12 +22,34 @@ $(document).ready(function() {
 		.mousemove(function(e) {
 
 			if(isDragging) {
-				tick++;
+				//tick++;
 
-				if (tick >= max_tick) {
+				let posX = $(this).position().left;
+		        let posY = $(this).position().top;
+		        let relative_posX = (e.pageX - posX);
+		        let relative_posY = (e.pageY - posY);
+
+				//if (tick >= max_tick) {
+
+				// check if elements are colliding
+				/*let displayable = true;
+				for (let i=0 ; i<elements_pos.length ; i++) {
+					let current_elements_posX = elements_pos[i][0];
+					let current_elements_posY = elements_pos[i][1];
+					console.log(current_elements_posX);
+					console.log(current_elements_posY);
+					if (relative_posX > current_elements_posX - space_between_elements
+						&& relative_posX < current_elements_posX + space_between_elements
+						&& relative_posY > current_elements_posY - space_between_elements
+						&& relative_posY < current_elements_posY + space_between_elements) {
+						console.log(false);
+						displayable = false;
+						break;
+					}
+				}*/
+
+				//if (displayable) {
 					// if last element print if max_tick away
-					let posX = $(this).position().left;
-			        let posY = $(this).position().top;
 
 			        // with circles
 					/*var svg = document.getElementsByTagName('clipPath')[0]; //Get svg element
@@ -46,15 +70,17 @@ $(document).ready(function() {
 
 					var svg = document.getElementsByTagName('clipPath')[0]; //Get svg element
 					var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'ellipse'); //Create a path in SVG's namespace
-					newElement.setAttribute("cx", (e.pageX - posX)); // set x position (relative to slide)
-					newElement.setAttribute("cy", (e.pageY - posY)); // set y position (relative to slide)
+					newElement.setAttribute("cx", relative_posX); // set x position (relative to slide)
+					newElement.setAttribute("cy", relative_posY); // set y position (relative to slide)
 					newElement.setAttribute("rx", circle_size-random_rx); // set size of circles
 					newElement.setAttribute("ry", circle_size+random_ry); // set size of circles
 					svg.appendChild(newElement);
 
+					//elements_pos.push([(e.pageX - posX), (e.pageY - posY)]);
+
 					// reset tick once printed
-					tick = 0;
-				}
+					//tick = 0;
+				//}
 			}
 
 		})
--- sorties-de-secours/public/js/index.js
+++ sorties-de-secours/public/js/index.js
@@ -1,17 +1,67 @@
-// init du slider (qui peut aussi dfaire des fondus enchainé)
-let mySlidr = slidr.create('slidr',{
-    breadcrumbs: false,
-    controls: 'none',
-    direction: 'vertical',
-    fade: true,
-    keyboard: true,
-    overflow: true,
-    pause: false,
-    theme: '#222',
-    timing: { 'fade': '0.5s ease-in' },
-    touch: true,
-    transition: 'fade'
-  }).start();
+/* =========================
+    FULLPAGE INITIALISATION
+   ========================= */
 
-// on s'occupe de la 1ère slide
-initSlide1();
+// variables
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+// toggle menu 
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionSelector: '.vertical-scrolling',
+  slideSelector: '.horizontal-scrolling',
+  navigation: false,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['home', 'secondSection', 'thirdSection', 'expo', 'dance'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(false, 'up');
+      $header_top.css('background', 'transparent');
+      $nav.css('background', 'transparent');
+      $(this).css('background', '#374140');
+      $(this).find('h2').css('color', 'white');
+      $(this).find('h3').css('color', 'white');
+      $(this).find('p').css(
+        {
+          'color': '#DC3522',
+          'opacity': 1,
+          'transform': 'translateY(0)'
+        }
+      );
+    }
+  },
+
+  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(true, 'up');
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+    }
+  } 
+});
+/* =============================
+    END FULLPAGE INITIALISATION
+   ============================= */
