63c93f91feaac8e3a182f8f3e525ed3af271489a
Christophe <contact@christophe-sonneville.fr>
2020-01-28 19:39:16 +0100

    switch charactere color

--- handiplanet/public/views/view1/view1.js
+++ handiplanet/public/views/view1/view1.js
@@ -25,7 +25,41 @@ class View1 extends View {
     linkElements(){
         this.btNext = document.getElementById('view-1-next-button');
         this.btNext.addEventListener('click', ()=>{
+            this.btNext.style.display = 'none';
             this.switchToView2();
         });
+
+        // Switch color of character
+        this.switchCharButton = document.getElementById('view-1-switch-character');
+        this.switchCharButton.addEventListener('click', () => {
+            this.showSelectChar();
+        });
+
+        // Select color of character
+        this.selectCharContainer = document.getElementById('view-1-select-character');
+        this.caracterImg = document.getElementById('view-1-charactere-img');
+
+        this.selectCharButtons = document.querySelectorAll('#view-1-select-character li');
+
+        // Change svg of character
+        const srcCharSvg = ['character-white-view1.svg', 'character-view1.svg', 'character-red-view1.svg'];
+        this.selectCharButtons.forEach((li, index) => {
+            li.addEventListener('click', () => {
+                this.caracterImg.src = `/handiplanet/assets/${srcCharSvg[index]}`;
+                this.hideSelectChar();
+            });
+        });
+    }
+
+    showSelectChar() {
+        // Show color selector
+        this.switchCharButton.style.display = 'none';
+        this.selectCharContainer.style.display = 'initial';
+    }
+
+    hideSelectChar() {
+        // Hide color selector
+        this.selectCharContainer.style.display = 'none';
+        this.switchCharButton.style.display = 'initial';
     }
 }
