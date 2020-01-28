7e15eb0edd63c30cc8e0496d1ff0644a48b583e9
Guillaume KERGREIS <guillaume.kergreis@etu.univ-nantes.fr>
2020-01-28 16:03:42 +0100

    Ajoute la gestion des étapes (suivant, témoignages, ...)

--- sciencescom/server.js
+++ sciencescom/server.js
@@ -10,6 +10,11 @@ let app = express();
 // Minimum routing: serve static content from the html directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../__common-logos__')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', (req, res, next) => res.render('index'));
 
 // You can then add whatever routing code you need
 
