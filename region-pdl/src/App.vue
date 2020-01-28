e75301ca0f793bc949920ff189f4498ea1c32464
Julien Garcia <juliennoal.garcia@etu.univ-nantes.fr>
2020-01-28 14:36:44 +0100

    Ajout du nuancier de couleurs en classes dans App.vue

--- region-pdl/src/App.vue
+++ region-pdl/src/App.vue
@@ -11,4 +11,42 @@ html, body, #app, .mainScene {
   height: 100vh;
   width: 100vw;
 }
+
+/* --- Couleurs du nuancier --- */
+.bg-darkblue {
+  background-color: #2c4068;
+}
+.darkblue {
+  color: #2c4068;
+}
+.bg-bluegreen {
+  background-color: #63b4bb;
+}
+.bluegreen {
+  color: #63b4bb;
+}
+.bg-green {
+  background-color: #d6efbb;
+}
+.green {
+  color: #d6efbb;
+}
+.bg-orange {
+  background-color: #fd7e61;
+}
+.orange {
+  color: #fd7e61;
+}
+.bg-grey {
+  background-color: #e3deca;
+}
+.grey {
+  color: #e3deca;
+}
+.bg-cream {
+  background-color: #fcf8ef;
+}
+.cream {
+  color: #fcf8ef;
+}
 </style>
