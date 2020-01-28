f1fc85cba0d10d1852cc6f571ab73989cd365c16
Pierre Maupin <pierre.maupin@etu.univ-nantes.fr>
2020-01-28 16:12:14 +0100

    text size & sound

--- handiplanet/public/js/index.js
+++ handiplanet/public/js/index.js
@@ -14,13 +14,44 @@ window.addEventListener('load', function () {
     }
 
     function setSmall(){
+        small.style.background = '#cb563e';
+        medium.style.background = '#5a8d8c';
+        large.style.background = '#5a8d8c';
 
+        //mettre les textes en small
+        text.style.fontSize = "50%" ;
     }
     function setMedium(){
+        medium.style.background = '#cb563e';
+        small.style.background = '#5a8d8c';
+        large.style.background = '#5a8d8c';
 
+        //mettre les textes en medium
+        text.style.fontSize = "100%" ;
     }
     function setLarge(){
+        large.style.background = '#cb563e';
+        small.style.background = '#5a8d8c';
+        medium.style.background = '#5a8d8c';
 
+        //mettre les textes en larges
+        text.style.fontSize = "150%" ;
+    }
+
+    function mute(){
+        sonMuteB.style.display = 'block';
+        sonOnB.style.display = 'none';
+        sonMuteW.style.display = 'block';
+        sonOnW.style.display = 'none';
+        //enlever son
+
+    }
+    function on(){
+        sonMuteB.style.display = 'none';
+        sonOnB.style.display = 'block';
+        sonMuteW.style.display = 'none';
+        sonOnW.style.display = 'block';
+        //mettre son
     }
 
 
@@ -33,4 +64,17 @@ window.addEventListener('load', function () {
     medium.onclick = setMedium;
     var large = document.getElementById('large');
     large.onclick = setLarge;
+
+    
+    var sonOnB = document.getElementById('onB');
+    sonOnB.onclick = mute;
+    var sonMuteB = document.getElementById('muteB');
+    sonMuteB.onclick = on;
+
+    var sonOnW = document.getElementById('onW');
+    sonOnW.onclick = mute;
+    var sonMuteW = document.getElementById('muteW');
+    sonMuteW.onclick = on;
+
+    text = document.getElementsByClassName('')
 });	
