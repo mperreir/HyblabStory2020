08ac68bceb20421259c1bc4b90a6a2a7da8cca19
Julien Le Roux <julien.le-roux@etu.univ-nantes.fr>
2020-01-28 14:28:50 +0100

    [ajout slide 11]

--- ville-saint-nazaire/public/js/slide11.js
+++ ville-saint-nazaire/public/js/slide11.js
@@ -4,4 +4,14 @@ let initSlide11 = function(){
     mySlidr.slide('page-12');
     initSlide12();
   });
+
+  d3.select('#next13').on('click', function(){
+    mySlidr.slide('page-13');
+    initSlide13();
+  });
+
+  d3.select('#next14').on('click', function(){
+    mySlidr.slide('page-14');
+    initSlide14();
+  });
 }
