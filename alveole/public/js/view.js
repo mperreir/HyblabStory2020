b7a696030a738a3bd6db3d3700aafcb9eb2d71ae
ShikoteiCoding <business.vitis@gmail.com>
2020-01-30 16:50:39 +0100

    Footer svg with Snap.js

--- alveole/public/js/view.js
+++ alveole/public/js/view.js
@@ -14,7 +14,18 @@ class ViewFooter {
     this.main = document.querySelector('main');
     let footer = document.createElement('footer');
 
+    let div = document.createElement('div');
+    div.setAttribute('id', 'footer_ellipse');
+
+    footer.appendChild(div);
+
+    Snap.load('data/footer_ellipse.svg', function(data) {
+      let snap = Snap('#footer_ellipse');
+      snap.append(data);
+    });
+
     this.main.appendChild(footer);
+
   }
 }
 
