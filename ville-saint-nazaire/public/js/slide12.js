6ad5a18872fecd970ffb1db656ca6fe994a5b4d2
E18A591G <ghislain.percie-du-sert@etu.univ-nantes.fr>
2020-01-31 14:23:52 +0100

    Création fichier util.js pour factoriser les fonctions

--- ville-saint-nazaire/public/js/slide12.js
+++ ville-saint-nazaire/public/js/slide12.js
@@ -5,3 +5,23 @@ let initSlide12 = function(){
     initSlide15();
   });
 }
+
+function moveBureau12(){
+  myMove(document.getElementById('image_bureau12'),-20);
+}
+function moveCommission12(){
+  myMove(document.getElementById('image_commission12'),-20);
+}
+function moveSalon12(){
+  myMove(document.getElementById('image_salon12'),-12);
+}
+
+function returnBureau12(){
+  myReturn(document.getElementById('image_bureau12'),20);
+}
+function returnCommission12(){
+  myReturn(document.getElementById('image_commission12'),20);
+}
+function returnSalon12(){
+  myReturn(document.getElementById('image_salon12'),12);
+}
--- ville-saint-nazaire/public/js/slide14.js
+++ ville-saint-nazaire/public/js/slide14.js
@@ -5,3 +5,23 @@ let initSlide14 = function(){
     initSlide15();
   });
 }
+
+function moveBureau14(){
+  myMove(document.getElementById('image_bureau14'),-20);
+}
+function moveCommission14(){
+  myMove(document.getElementById('image_commission14'),-20);
+}
+function moveSalon14(){
+  myMove(document.getElementById('image_salon14'),-12);
+}
+
+function returnBureau14(){
+  myReturn(document.getElementById('image_bureau14'),20);
+}
+function returnCommission14(){
+  myReturn(document.getElementById('image_commission14'),20);
+}
+function returnSalon14(){
+  myReturn(document.getElementById('image_salon14'),12);
+}
