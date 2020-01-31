1c273c095390fc937bfa754562dbeef0230741fe
E18A591G <ghislain.percie-du-sert@etu.univ-nantes.fr>
2020-01-31 10:11:39 +0100

    Mouvement images slide 9

--- ville-saint-nazaire/public/js/slide9.js
+++ ville-saint-nazaire/public/js/slide9.js
@@ -5,3 +5,53 @@ let initSlide9 = function(){
     initSlide10();
   });
 }
+
+function moveBureau(){
+  myMove(document.getElementById('image_bureau'),-20);
+}
+function moveCommission(){
+  myMove(document.getElementById('image_commission'),-20);
+}
+function moveSalon(){
+  myMove(document.getElementById('image_salon'),-12);
+}
+
+function returnBureau(){
+  myReturn(document.getElementById('image_bureau'),20);
+}
+function returnCommission(){
+  myReturn(document.getElementById('image_commission'),20);
+}
+function returnSalon(){
+  myReturn(document.getElementById('image_salon'),12);
+}
+
+
+
+function myMove(elem,posInitiale) {
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == -posInitiale) { //condition d'arrêt de la "boucle"
+      clearInterval(id);
+    } else {
+      pos++;
+      //elem.style.top = (pos+30) + '%';//déplacement vers le bas
+      elem.style.left = (pos+posInitiale) + '%';//déplacement vers la gauche
+    }
+  }
+}
+
+function myReturn(elem,posInitiale) {
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == -posInitiale) { //condition d'arrêt de la "boucle"
+      clearInterval(id);
+    } else {
+      pos--;
+      //elem.style.top = (pos+30) + '%';//déplacement vers le bas
+      elem.style.left = (pos) + '%';//déplacement vers la gauche
+    }
+  }
+}
