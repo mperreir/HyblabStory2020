0f871e9b337e2ece6a0e35fcac2e73fbb0c6b111
Nathan Seva <thykof@protonmail.ch>
2020-02-03 15:56:52 +0100

    WIP

--- region-pdl/src/components/Saynettes/Maison.vue
+++ region-pdl/src/components/Saynettes/Maison.vue
@@ -2,18 +2,21 @@
   <div class="saynette">
     <MaisonSVG class="bg-bluegreen svg" />
     <Cloud1 class="cloud" />
+    <House class="house" />
   </div>
 </template>
 
 <script>
 import MaisonSVG from "@/assets/HOME/HOME FOND_Plan de travail 1.svg";
 import Cloud1 from "@/assets/HOME/Nuage 1Fichier 7.svg";
+import House from "@/assets/house.svg";
 
 export default {
     name: "Maison",
     components: {
         MaisonSVG,
-        Cloud1
+        Cloud1,
+        House
     },
     data: () => ({
         classObject: {
@@ -44,5 +47,14 @@ export default {
     width: 10%;
     color: red;
     font-size: 3vw;
+}
+
+.house {
+  position: absolute;
+  top: 25%;
+  left: 20%;
+  width: 10%;
+  color: red;
+  font-size: 3vw;
 }
 </style>
