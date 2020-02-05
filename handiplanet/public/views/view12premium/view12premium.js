2d39e26fe31b42da0c275ad6af625ef871e4c0c6
Pierre Le Guen <pierre@leguen.fr>
2020-02-05 22:22:13 +0100

    view12-rename

--- handiplanet/public/views/view12premium/view12premium.js
+++ handiplanet/public/views/view12premium/view12premium.js
@@ -3,15 +3,15 @@
  * Created Date: Wednesday February 5th 2020
  * Author: Pierre Le Guen
  * -----
- * Last Modified: Wednesday, 5th February 2020 9:40:47 pm
+ * Last Modified: Wednesday, 5th February 2020 10:20:36 pm
  * Modified By: Pierre Le Guen
  * -----
  * Copyright (c) 2020 Pierre Le Guen
  */
 
-class View12 extends View {
+class View12Premium extends View {
     constructor(el) {
-      super('view12', el || document.getElementById('view-container'));
+      super('view12premium', el || document.getElementById('view-container'));
       this.load()
         .then(() => {
           this.view = document.getElementById(this.viewName);
