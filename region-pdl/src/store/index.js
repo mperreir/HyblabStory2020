89fbacbad27e3723851be841eacac1058b7f797b
Nathan Seva <thykof@protonmail.ch>
2020-02-06 18:35:19 +0100

    Fix store

--- region-pdl/src/store/index.js
+++ region-pdl/src/store/index.js
@@ -5,8 +5,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentSceneIndex: 4,
-    nbScenes: 8
+    currentSceneIndex: 0,
+    nbScenes: 8,
+    character: ""
   },
   mutations: {
       nextScene(state, sceneId) {
