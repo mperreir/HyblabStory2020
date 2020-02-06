c8c71765423ec40a6beb849df8d419cd8e533eb6
Christophe <contact@christophe-sonneville.fr>
2020-02-06 16:56:17 +0100

    test work

--- handiplanet/public/js/index.js
+++ handiplanet/public/js/index.js
@@ -9,6 +9,7 @@ window.scrollBarController.setPosition(0);
 
 view = new View1(document.getElementById('view-container'));
 
+
 window.addEventListener('load', function () {
     function scroll(){
         window.scrollTo(0,window.innerHeight);
