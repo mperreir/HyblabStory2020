2dcbcb329772e9db98c4e012774b1bc6f43ba77c
Simon BELLANGER <simon.bellanger1@etu.univ-nantes.fr>
2020-02-06 13:55:47 +0100

    Modif slides 2, 13 et 14

--- ville-saint-nazaire/public/js/slide2.js
+++ ville-saint-nazaire/public/js/slide2.js
@@ -8,15 +8,24 @@ let initSlide2 = function(){
         initSlide3();
     };
 
-    d3.select('#next3').on('click', function(){
-      console.log('3');
+    d3.select('.next3').on('click', function(){
       son.pause();
       moveDown();
       initSlide3();
     });
 
+    d3.select('.next3').on('mouseover', function(){
+        anime({
+            targets: '#next3-texte, #next3-img',
+            scale: 1.2
+        });
+    });
 
-
-    //setTimeout(next3, 16000);
+    d3.select('.next3').on('mouseout', function(){
+        anime({
+            targets: '#next3-texte, #next3-img',
+            scale: 1
+        });
+    });
 
 }
