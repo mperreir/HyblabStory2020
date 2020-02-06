37d67c4fecd5f2b78e444f990bd783a8290d8e0a
Pierre Le Guen <pierre@leguen.fr>
2020-02-06 10:18:29 +0100

    view12- add animation

--- handiplanet/public/views/view12gratuit/view12gratuit.js
+++ handiplanet/public/views/view12gratuit/view12gratuit.js
@@ -3,23 +3,54 @@
  * Created Date: Wednesday February 5th 2020
  * Author: Pierre Le Guen
  * -----
- * Last Modified: Wednesday, 5th February 2020 10:23:44 pm
+ * Last Modified: Thursday, 6th February 2020 10:18:02 am
  * Modified By: Pierre Le Guen
  * -----
  * Copyright (c) 2020 Pierre Le Guen
  */
 
 class View12Gratuit extends View {
-    constructor(el) {
-      super('view12gratuit', el || document.getElementById('view-container'));
-      this.load()
-        .then(() => {
-          this.view = document.getElementById(this.viewName);
-          this.linkElements();
-        });
-    }
-  
-    linkElements() {
-      
-    }
+  constructor (el) {
+    super('view12gratuit', el || document.getElementById('view-container'))
+    this.load().then(() => {
+      this.view = document.getElementById(this.viewName)
+      this.linkElements()
+    })
   }
+
+  linkElements () {
+    // Background elements
+    this.interrogation1 = document.getElementById('interrogation1')
+    this.interrogation2 = document.getElementById('interrogation2')
+    this.loupe = document.getElementById('loupe')
+    this.ellipse = document.getElementById('ellipse')
+
+    // Parallax for background elements
+    const vc = document.getElementById('view-container')
+    vc.addEventListener('mousemove', e => {
+      var relX = e.pageX - vc.offsetLeft
+      var relY = e.pageY - vc.offsetTop
+
+      TweenMax.to(this.interrogation1, 1, {
+        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * -10,
+        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * -10,
+        ease: Power2.easeOut
+      })
+      TweenMax.to(this.interrogation2, 1, {
+        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * 10,
+        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * -10,
+        ease: Power2.easeOut
+      })
+      TweenMax.to(this.loupe, 1, {
+        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * -10,
+        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * 10,
+        ease: Power2.easeOut
+      })
+      TweenMax.to(this.ellipse, 1, {
+        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * 10,
+        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * 10,
+        ease: Power2.easeOut
+      })
+    })
+  }
+}
