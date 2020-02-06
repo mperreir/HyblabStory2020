1b56af89512a0c475c6e0cd0490c50f89d0956b5
Julien Le Roux <julien.le-roux@etu.univ-nantes.fr>
2020-02-06 13:59:15 +0100

    [modif slide 11]

--- ville-saint-nazaire/public/js/slide11.js
+++ ville-saint-nazaire/public/js/slide11.js
@@ -1,75 +1,29 @@
 let initSlide11 = function(){
 
 
-  function myMove(id,top1,left1) {
-
+  function set(id){
     var elem = document.getElementById(id);
-
-    var top = 0;
-    var left = 50;
-
-    let coeff1 = (top1)/8;
-    let coeff2 = (left1-50)/8;
-
-    if(left1<=50){
-      var id = setInterval(frame, 100);
-
-      function frame() {
-
-        if (top >= top1) {
-          clearInterval(id);
-        } else {
-          top=top+coeff1;
-          left=left+coeff2;
-          elem.style.top = top + '%';
-          elem.style.left = left + '%';
-        }
-      }
-    }else if(left1>50){
-      var id = setInterval(frame, 100);
-
-      function frame() {
-
-        if (top >= top1) {
-          clearInterval(id);
-        } else {
-          top=top+coeff1;
-          left=left+coeff2;
-          elem.style.top = top + '%';
-          elem.style.left = left + '%';
-        }
-      }
-
-
-    }
+    elem.style.visibility='visible';
   }
 
 
-  myMove("maq1_1",45,14)
-  myMove("maq1_2",53,10.2)
-  myMove("maq1_3",34,12)
-  myMove("maq1_4",38,6.5)
-  myMove("maq1_5",43,5)
-  myMove("maq1_6",39.5,20)
-  myMove("maq1_7",59,21.5)
+  function display(id, callback = null) {
+    setTimeout(function() {
+      document.getElementById(id).style.visibility = "visible"
+      if (callback != null) {
+        callback()
+      }
+    }, 200)
+  }
+ 
+ 
+ 
+  display("maq1_1", () => display("maq1_2", () => display("maq1_3", () => display("maq1_4",() => display("maq1_5",() => display("maq1_6",() => display("maq1_7")))))))
 
+  display("maq2_1", () => display("maq2_2", () => display("maq2_3", () => display("maq2_4",() => display("maq2_5",() => display("maq2_6",() => display("maq2_7")))))))
 
-  myMove("maq2_1",47,46)
-  myMove("maq2_2",41,50)
-  myMove("maq2_3",45,41)
-  myMove("maq2_4",55,53)
-  myMove("maq2_5",58,45)
-  myMove("maq2_6",34,44)
-  myMove("maq2_7",35,42)
+  display("maq3_1", () => display("maq3_2", () => display("maq3_3", () => display("maq3_4",() => display("maq3_5",() => display("maq3_6",() => display("maq3_7")))))))
 
-
-  myMove("maq3_1",48,81)
-  myMove("maq3_2",58,78.3)
-  myMove("maq3_3",38.5,76.5)
-  myMove("maq3_4",54,69)
-  //myMove("maq3_5",46.7,70)
-  myMove("maq3_6",33,83)
-  myMove("maq3_7",35.5,74.7)
 
 
 
