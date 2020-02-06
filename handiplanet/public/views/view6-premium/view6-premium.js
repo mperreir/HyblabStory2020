25c04d399e60f7f30f320809590a1bc361bc366a
Eric Bizet <eric.bizet@etu.univ-nantes.fr>
2020-02-06 16:32:25 +0100

    Debut integration js next slide

--- handiplanet/public/views/view6-premium/view6-premium.js
+++ handiplanet/public/views/view6-premium/view6-premium.js
@@ -9,8 +9,14 @@ class View6Premium extends View {
         this.smartphone = document.getElementById('view6premium-smartphone');
         this.btn_like.addEventListener('click', ()=>{
             this.smartphone.src = "./assets/view6-premium/smartphone-clicked.svg";
-            console.log("click");
         });
-        console.log("test");
+
+        linkElements(){
+            // Button to next view
+            this.btNext = document.getElementById('view6p-next-button');
+            this.btNext.addEventListener('click', ()=>{
+                
+            });
+        }
     }
 }
