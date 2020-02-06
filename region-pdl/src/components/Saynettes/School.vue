da6a4e86887543c7f1fd47dc19d2e297874d318e
Mathis Le Berrigaud <mathis.le-berrigaud@etu.univ-nantes.fr>
2020-02-06 09:50:23 +0100

    creation textes saynette ecole

--- region-pdl/src/components/Saynettes/School.vue
+++ region-pdl/src/components/Saynettes/School.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="saynette">
+  <TextTitle :x="7" :y="30">Se chauffer (aides isolation professionnelles)</TextTitle>
+    <SimpleText :x="7" :y="43" :width="40">
+        M. Moreau est professeur des écoles. Cette année, son école a bénéficié d’une aide de la Région pour réaliser des travaux d’isolation. 
+    </SimpleText>
     <Background class="svg" />
   </div>
 </template>
 
 <script>
 import Background from "@/assets/School/background-school.svg";
+import TextTitle from "@/components/TextTitle";
+import SimpleText from "@/components/SimpleText";
 
 export default {
   name: "School",
   components: {
-    Background
+    Background,
+    TextTitle,
+    SimpleText
   },
   data: () => ({}),
   methods: {}
