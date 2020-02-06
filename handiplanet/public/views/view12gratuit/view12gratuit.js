73282cbec53d76b4cd457ed2b592abbb0408a970
Pierre Le Guen <pierre@leguen.fr>
2020-02-06 10:59:29 +0100

    view12-infinite anim

--- handiplanet/public/views/view12gratuit/view12gratuit.js
+++ handiplanet/public/views/view12gratuit/view12gratuit.js
@@ -3,7 +3,7 @@
  * Created Date: Wednesday February 5th 2020
  * Author: Pierre Le Guen
  * -----
- * Last Modified: Thursday, 6th February 2020 10:18:02 am
+ * Last Modified: Thursday, 6th February 2020 10:44:52 am
  * Modified By: Pierre Le Guen
  * -----
  * Copyright (c) 2020 Pierre Le Guen
@@ -20,9 +20,6 @@ class View12Gratuit extends View {
 
   linkElements () {
     // Background elements
-    this.interrogation1 = document.getElementById('interrogation1')
-    this.interrogation2 = document.getElementById('interrogation2')
-    this.loupe = document.getElementById('loupe')
     this.ellipse = document.getElementById('ellipse')
 
     // Parallax for background elements
@@ -30,22 +27,6 @@ class View12Gratuit extends View {
     vc.addEventListener('mousemove', e => {
       var relX = e.pageX - vc.offsetLeft
       var relY = e.pageY - vc.offsetTop
-
-      TweenMax.to(this.interrogation1, 1, {
-        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * -10,
-        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * -10,
-        ease: Power2.easeOut
-      })
-      TweenMax.to(this.interrogation2, 1, {
-        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * 10,
-        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * -10,
-        ease: Power2.easeOut
-      })
-      TweenMax.to(this.loupe, 1, {
-        x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * -10,
-        y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * 10,
-        ease: Power2.easeOut
-      })
       TweenMax.to(this.ellipse, 1, {
         x: ((relX - vc.offsetWidth / 2) / vc.offsetWidth) * 10,
         y: ((relY - vc.offsetHeight / 2) / vc.offsetHeight) * 10,
