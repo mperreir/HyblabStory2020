b85a4b67e855466a90ab5b8529f81b05b5629661
Jonas Delannoy <e165556c@etu.univ-nantes.fr>
2020-02-06 09:50:00 +0100

    correctifs

--- sorties-de-secours/public/js/index.js
+++ sorties-de-secours/public/js/index.js
@@ -1,7 +1,7 @@
 // variables
 var $header_top = $('.header-top');
 var $nav = $('nav');
-var position = $(window).scrollTop();
+var iScrollPos = 0;
 
 // toggle menu
 $header_top.find('a').on('click', function() {
@@ -20,12 +20,16 @@ function enableScroll() {
   window.removeEventListener('scroll', noScroll);
 }
 
-disableScroll();
-
 function goToByScroll(id) {
-  enableScroll();
   $('html,body').animate({
       scrollTop: $("#" + id).offset().top
   }, 400);
-  disableScroll();
 }
+
+//Desactivation du scroll horizontal
+var $body = $(document);
+$body.bind('scroll', function() {
+    if ($body.scrollLeft() !== 0) {
+        $body.scrollLeft(0);
+    }
+});
