4cffc818c0840ac5d92f5691bfb808481b8649bf
Pierre Le Guen <pierre@leguen.fr>
2020-02-06 14:33:55 +0100

    add copyrights

--- handiplanet/public/views/view10/view10.js
+++ handiplanet/public/views/view10/view10.js
@@ -1,3 +1,14 @@
+/*
+ * Project: handigo
+ * Created Date: Wednesday February 5th 2020
+ * Author: Pierre Le Guen
+ * -----
+ * Last Modified: Thursday, 6th February 2020 2:33:29 pm
+ * Modified By: Pierre Le Guen
+ * -----
+ * Copyright (c) 2020 Pierre Le Guen
+ */
+
 class View10 extends View {
   constructor(el) {
     super('view10', el || document.getElementById('view-container'));
