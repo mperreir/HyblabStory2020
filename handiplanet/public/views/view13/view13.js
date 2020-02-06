b43c9624c8a01ae5edbb1a009950405f969bc7eb
Pierre Le Guen <pierre@leguen.fr>
2020-02-06 16:44:34 +0100

    view13-add graph & style

--- handiplanet/public/views/view13/view13.js
+++ handiplanet/public/views/view13/view13.js
@@ -1,3 +1,14 @@
+/*
+ * Project: handigo
+ * Created Date: Thursday February 6th 2020
+ * Author: Pierre Le Guen
+ * -----
+ * Last Modified: Thursday, 6th February 2020 4:34:08 pm
+ * Modified By: Pierre Le Guen
+ * -----
+ * Copyright (c) 2020 Pierre Le Guen
+ */
+
 
 class View13 extends View {
     constructor(receiver){
