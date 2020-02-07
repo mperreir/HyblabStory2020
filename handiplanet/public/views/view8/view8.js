4443d5fd48fd4dc0e268e596e76a52e9fd5985a9
Alban Lecuivre <lecuivre.alban@gmail.com>
2020-02-07 15:21:00 +0100

    refactor transitions & update contexte

--- handiplanet/public/views/view8/view8.js
+++ handiplanet/public/views/view8/view8.js
@@ -26,48 +26,10 @@ class View8 extends View {
   }
 
   async switchToView9Premium() {
-    // Construction d'une div temporaire positionnée en dehors
-    // de l'écran pour faire entrer la prochaine view
-    const tempDiv = document.createElement('div');
-    document.getElementById('view-container').appendChild(tempDiv);
-    tempDiv.style.position = "absolute";
-    tempDiv.style.top = '0';
-    tempDiv.style.left = '100%';
-
-    // Création de la prochaine view
-    const nextView = new View9Premium(tempDiv);
-    await nextView.load();
-    this.view.style.animation = 'scrollTransitionHorizontal 1s forwards';
-    this.view.style.webkitAnimation = 'scrollTransitionHorizontal 1s forwards';
-    tempDiv.style.animation = 'scrollTransitionHorizontal 1s forwards';
-    tempDiv.style.webkitAnimation = 'scrollTransitionHorizontal 1s forwards';
-    window.scrollBarController.setPosition(1);
-    setTimeout(() => {
-      tempDiv.replaceWith = nextView.view;
-      this.view.parentNode.removeChild(this.view);
-    }, 1000);
+    transitionHorizontal(this.view, View9Premium);
   }
 
   async switchToView9Free() {
-    // Construction d'une div temporaire positionnée en dehors
-    // de l'écran pour faire entrer la prochaine view
-    const tempDiv = document.createElement('div');
-    document.getElementById('view-container').appendChild(tempDiv);
-    tempDiv.style.position = "absolute";
-    tempDiv.style.top = '0';
-    tempDiv.style.left = '100%';
-
-    // Création de la prochaine view
-    const nextView = new View9Free(tempDiv);
-    await nextView.load();
-    this.view.style.animation = 'scrollTransitionHorizontal 1s forwards';
-    this.view.style.webkitAnimation = 'scrollTransitionHorizontal 1s forwards';
-    tempDiv.style.animation = 'scrollTransitionHorizontal 1s forwards';
-    tempDiv.style.webkitAnimation = 'scrollTransitionHorizontal 1s forwards';
-    window.scrollBarController.setPosition(1);
-    setTimeout(() => {
-      tempDiv.replaceWith = nextView.view;
-      this.view.parentNode.removeChild(this.view);
-    }, 1000);
+    transitionHorizontal(this.view, View9Free);
   }
 }
