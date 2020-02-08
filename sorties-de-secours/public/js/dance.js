ae36bc2ba8276924f17f4ed49f088e9c2b027d3f
E165556C <jonas.delannoy@etu.univ-nantes.fr>
2020-02-08 23:02:23 +0100

    Experience Dance (PaperJs) + des trucs

--- sorties-de-secours/public/js/dance.js
+++ sorties-de-secours/public/js/dance.js
@@ -1,15 +1,39 @@
 $(document).ready(function() {
-  $("body").scroll(function() {
-    console.log("scrolled");
-    $("#img-dancers-annotations").css("opacity", "1");
-  });
-  $('body').bind('mousewheel DOMMouseScroll', function (e) {
-    //console.log('bingo');
+
+  var dancePosTop = {
+    top: $('#dance-section').offset().top,
+    bottom: $('#dance-section').offset().top + $('#dance-section').height()
+  };
+
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() > dancePosTop.top) {
+      bodyScrollLock.disableBodyScroll("#container");
+      goToByScroll("dance-section");
+      $("#dance-first-slide").css("background-color", "white");
+      setTimeout(function() {
+        const mouseAnimated = document.querySelector("#mouse-animated-dance");
+        mouseAnimated.classList.remove('fadeOut');
+        mouseAnimated.classList.add('fadeInUp');
+        isExperience = false;
+      }, 10000);
+    }
   });
 
-  $("#dance-section").css("overflow-x", "scroll");
+  let isExperience = true;
+
+
+
   $('#dance-section').on('mousewheel', function(e, delta) {
-    this.scrollLeft -= (delta * 60);
+    if(!isExperience) {
+      $("#img-dancers-2").css("opacity", "1");
+      /*this.scrollLeft -= (delta * 60);*/
+    }
+
+
     //e.preventDefault();
   });
+
 });
+
+/* DANCE EXPERIENCE */
