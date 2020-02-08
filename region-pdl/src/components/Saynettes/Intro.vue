bc79d946de31d65a48b3c162074a68245f1a9063
Nathan Seva <thykof@protonmail.ch>
2020-02-08 13:26:21 +0100

    Fix import svg home

--- region-pdl/src/components/Saynettes/Intro.vue
+++ region-pdl/src/components/Saynettes/Intro.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="saynette">
-    <TextTitle :x="7" :y="30">
+    <TextTitle
+      :x="7"
+      :y="30"
+    >
       La Région Pays de la Loire
-      <br />pour la transition énergétique
+      <br>pour la transition énergétique
     </TextTitle>
-    <SimpleText :x="7" :y="43" :width="40">
+    <SimpleText
+      :x="7"
+      :y="43"
+      :width="40"
+    >
       Suivez la journée de Monsieur Moreau et Madame Dubois
-      <br />et découvrez comment la
+      <br>et découvrez comment la
       <b>Région Pays de La Loire</b>
-      <br />les accompagne pour une consommation d'énergie plus verte.
+      <br>les accompagne pour une consommation d'énergie plus verte.
     </SimpleText>
-    <SimpleButton text="Démarrer l'aventure" :width="23" :x="7" :y="58" @click.native="onNext" />
+    <SimpleButton
+      text="Démarrer l'aventure"
+      :width="23"
+      :x="7"
+      :y="58"
+      @click.native="onNext"
+    />
     <MaisonSVG class="svg" />
   </div>
 </template>
 
 <script>
-import MaisonSVG from "@/assets/Home/FOND HOME-01.svg";
+import MaisonSVG from "@/assets/HOME/FOND HOME-01.svg";
 import TextTitle from "@/components/TextTitle";
 import SimpleText from "@/components/SimpleText";
 import SimpleButton from "@/components/SimpleButton";
