11d62f0fef13ef56d863a917e595f64c566a7ac0
Alan Le Verge <alan.le-verge@etu.univ-nantes.fr>
2020-02-08 20:13:37 +0100

    ajout slide choix 3 + modif slide 10

--- ville-saint-nazaire/public/js/slide8.js
+++ ville-saint-nazaire/public/js/slide8.js
@@ -1,5 +1,36 @@
 let initSlide8 = function(){
 
+	// Animations bouton suivant
+	d3.select('#slide8-suivant').on('mouseover', function(){
+        anime({
+            targets: '#slide8-notesSuivantes, #slide8-btnSuivant',
+            scale: 1.1
+        });
+    });
+
+    d3.select('#slide8-suivant').on('mouseout', function(){
+        anime({
+            targets:  '#slide8-notesSuivantes, #slide8-btnSuivant',            
+            scale: 1
+        });
+    });
+
+
+    // Animations bouton retour
+	d3.select('#slide8-btnRetour').on('mouseover', function(){
+        anime({
+            targets: '#slide8-btnRetour',
+            scale: 1.1
+        });
+    });
+
+    d3.select('#slide8-btnRetour').on('mouseout', function(){
+        anime({
+            targets:  '#slide8-btnRetour',            
+            scale: 1
+        });
+    });
+
   d3.select('#next9-3').on('click', function(){
     moveDown();
     moveLeft();
