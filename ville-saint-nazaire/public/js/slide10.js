774d48dabd00a5873e0fc2c10893716ce1820c0c
Alan Le Verge <alan.le-verge@etu.univ-nantes.fr>
2020-02-09 23:01:52 +0100

    ajout texte qui apparait lettre par lettre (à tester)

--- ville-saint-nazaire/public/js/slide10.js
+++ ville-saint-nazaire/public/js/slide10.js
@@ -4,6 +4,39 @@ let initSlide10 = function(){
 		var dialogueJ = document.querySelector('#slide10-container-dialogueJ');
 		var dialogueD = document.querySelector('.slide10-container-dialogueD');
 
+
+		// Fonction d'affiche des dialogues lettre par lettre
+		var cptLettre = 0 ;
+		var timeOut ;
+		function showLetters(id)
+		{
+		  //console.log(id);
+		  let htmlP = document.getElementById(id);
+		  let txt = htmlP.dataset.label;
+		  console.log(txt.length);
+		  if(cptLettre < txt.length)
+			{
+				if (txt[cptLettre] == '<') {
+					htmlP.innerHTML += `<br>`
+				}
+				else {
+					htmlP.innerHTML += txt[cptLettre] ;
+				}
+			  	timeOut = setTimeout(function() {
+			      showLetters(id);
+			    }, 40)
+			  cptLettre++;
+			}
+		  else
+			{
+			  clearTimeout(timeOut);
+			  console.log("end");
+			}
+		}
+
+
+		showLetters("slide10-dialogue1");
+
 	   d3.select('#slide10-suivant').on('click', function(){
 
 	    	if (dialogueJ.style.visibility != "hidden") {
@@ -12,6 +45,9 @@ let initSlide10 = function(){
 	    		trait.style.visibility = "hidden";
 	    		trait = document.querySelector('#slide10-trait-dialogueD');
 	    		trait.style.visibility = "visible";
+	    		cptLettre = 0 ;
+	    		clearTimeout(timeOut);
+	    		showLetters("slide10-dialogue2");
 	    	}
 
 	    	else if(dialogueD.style.visibility != "hidden") {
