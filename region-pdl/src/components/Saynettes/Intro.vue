9d65e0d2914703fb6d7fef41dd35d990be9e4402
Julien Garcia <juliennoal.garcia@etu.univ-nantes.fr>
2020-02-09 18:54:29 +0100

    Arbre intro animation + changement gradient ciel

--- region-pdl/src/components/Saynettes/Intro.vue
+++ region-pdl/src/components/Saynettes/Intro.vue
@@ -1,35 +1,24 @@
 <template>
   <div class="saynette">
     <Background class="svg" />
-    <TextTitle
-      :x="7"
-      :y="20"
-    >
+    <TextTitle :x="7" :y="20">
       La Région des Pays de la Loire
-      <br>pour la transition énergétique
+      <br />pour la transition énergétique
     </TextTitle>
-    <SimpleText
-      :x="7"
-      :y="35"
-      :width="45"
-    >
+    <SimpleText :x="7" :y="35" :width="45">
       Suivez la journée de Monsieur Moreau et Madame Dubois
-      <br>et découvrez comment la
+      <br />et découvrez comment la
       <b>Région des Pays de la Loire</b>
-      <br>les accompagne pour réduire leur impact énergétique et construire un avenir plus vert.
+      <br />les accompagne pour réduire leur impact énergétique et construire un avenir plus vert.
     </SimpleText>
-    <SimpleButton
-      text="Démarrer l'aventure"
-      :width="23"
-      :x="7"
-      :y="58"
-      @click.native="onNext"
-    />
+    <SimpleButton text="Démarrer l'aventure" :width="23" :x="7" :y="58" @click.native="onNext" />
+    <Tree class="tree" />
   </div>
 </template>
 
 <script>
 import Background from "@/assets/background-intro.svg";
+import Tree from "@/assets/Home/tree-home.svg";
 import TextTitle from "@/components/TextTitle";
 import SimpleText from "@/components/SimpleText";
 import SimpleButton from "@/components/SimpleButton";
@@ -38,6 +27,7 @@ export default {
   name: "Intro",
   components: {
     Background,
+    Tree,
     TextTitle,
     SimpleButton,
     SimpleText
@@ -49,3 +39,30 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.tree {
+  position: absolute;
+  top: 55%;
+  left: 24%;
+  width: 12%;
+  overflow: visible
+}
+
+.poly {
+  transform: translateY(0) rotate(-0.5deg);
+  /* -webkit-animation: anim 1s linear infinite;
+  -moz-animation: anim 1s linear infinite;
+  -ms-animation: anim 1s linear infinite; */
+  animation: anim 2s ease-in-out infinite alternate;
+}
+
+@keyframes anim {
+  form {
+    transform: translateY(0) rotate(-0.5deg);
+  }
+  from {
+    transform: translateY(1vh) rotate(0.5deg);
+  }
+}
+</style>
