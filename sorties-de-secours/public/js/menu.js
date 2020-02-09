5b2d5fc241e434dc4ba603b024277c8bdcf84ca2
E165556C <jonas.delannoy@etu.univ-nantes.fr>
2020-02-09 18:34:03 +0100

    navigation between experience

--- sorties-de-secours/public/js/menu.js
+++ sorties-de-secours/public/js/menu.js
@@ -4,100 +4,116 @@ $(document).ready(function() {
 
   var menuFixed = false;
   var $exp_selected = undefined;
-  var canScroll = false;
-  var countScroll = 0;
-  let isExperience = true;
-  let endAnimationDance = false;
+  var choicesId = ["expChoiceExpo", "expChoiceMusique", "expChoiceDance"];
 
-  $(".expChoice").click(function() {
-    bodyScrollLock.enableBodyScroll(menuSection); // enable to avoid mousewheel event
+  function choiceClick(id) {
+    $("#"+id).click(function() {
+      var canScroll = false;
+      var countScroll = 0;
+      let isExperience = true;
+      let endAnimationDance = false;
+      bodyScrollLock.clearAllBodyScrollLocks(); // enable to avoid mousewheel event
 
-    $(".expChoice").addClass('expChoiceHidden');
+      $(".expChoice").addClass('expChoiceHidden');
 
-    $exp_selected = $(this);
-    $exp_selected.removeClass("expChoiceHidden");
-    $exp_selected.addClass("expChoiceExpand");
+      $exp_selected = $(this);
+      $exp_selected.removeClass("expChoiceHidden");
+      $exp_selected.addClass("expChoiceExpand");
 
-    let mouseAnimated = document.querySelector("#mouse-animated-menuDance");;
-    setTimeout(function() {
-      mouseAnimated = document.querySelector("#mouse-animated-menuDance");
-      mouseAnimated.classList.remove('fadeOut');
-      mouseAnimated.classList.add('fadeInUp');
-      canScroll = true;
-    }, 5000);
-    $(window).scroll(function(e) {
-      if (!canScroll) {
-        $('html, body').stop().animate({
-          scrollTop: $("#menu-section").offset().top
-        }, 0);
-      } else if (countScroll < 1) {
-        mouseAnimated.classList.remove('fadeInUp');
-        mouseAnimated.classList.add('fadeOut');
-        $('html, body').stop().animate({
-          scrollTop: $("#menu-section").offset().top
-        }, 0);
-        let expId = $exp_selected.attr('id');
-        let top = 0;
+      let mouseAnimatedExpo = document.querySelector("#mouse-animated-menuExpo");;
+      let mouseAnimatedMusic = document.querySelector("#mouse-animated-menuMusic");;
+      let mouseAnimatedDance = document.querySelector("#mouse-animated-menuDance");;
+      setTimeout(function() {
+        mouseAnimatedExpo.classList.remove('fadeOut');
+        mouseAnimatedExpo.classList.add('fadeInUp');
+        mouseAnimatedMusic.classList.remove('fadeOut');
+        mouseAnimatedMusic.classList.add('fadeInUp');
+        mouseAnimatedDance.classList.remove('fadeOut');
+        mouseAnimatedDance.classList.add('fadeInUp');
 
-        switch(expId) {
-          case "expChoiceMusique":
-          top = $('#music-workshop-section').offset().top;
-          break;
-          case "expChoiceDance":
-          top = $('#dance-section').offset().top;
-          break;
-          case "expChoiceExpo":
-          top = $('#expo-section').offset().top;
-          break;
-        }
-        $exp_selected.addClass("expChoiceOut");
-        setTimeout(function() {
-          $('html, body').animate({
-            scrollTop: top
-          }, 0, function () {
-            // Transition Experience Dance
-            bodyScrollLock.disableBodyScroll("#container"); // enable to avoid mousewheel event
+        canScroll = true;
+      }, 5000);
+      $(window).scroll(function(e) {
+        if (!canScroll) {
+          $('html, body').stop().animate({
+            scrollTop: $("#menu-section").offset().top
+          }, 0);
+        } else if (countScroll < 1) {
+          mouseAnimatedExpo.classList.remove('fadeInUp');
+          mouseAnimatedExpo.classList.add('fadeOut');
+          mouseAnimatedMusic.classList.remove('fadeInUp');
+          mouseAnimatedMusic.classList.add('fadeOut');
+          mouseAnimatedDance.classList.remove('fadeInUp');
+          mouseAnimatedDance.classList.add('fadeOut');
+          $('html, body').stop().animate({
+            scrollTop: $("#menu-section").offset().top
+          }, 0);
+          let expId = $exp_selected.attr('id');
+          let top = 0;
 
-            if(expId === "expChoiceDance") {
+          switch(expId) {
+            case "expChoiceMusique":
+            top = $('#music-workshop-section').offset().top;
+            break;
+            case "expChoiceDance":
+            top = $('#dance-section').offset().top;
+            break;
+            case "expChoiceExpo":
+            top = $('#expo-section').offset().top;
+            break;
+          }
+          $exp_selected.addClass("expChoiceOut");
+          setTimeout(function() {
+            $('html, body').animate({
+              scrollTop: top
+            }, 0, function () {
+              // Transition Experience Dance
+              bodyScrollLock.disableBodyScroll("#container"); // enable to avoid mousewheel event
 
-              $("#dance-first-slide").css("background-color", "white");
-              $("#img-dancers").css("opacity", "1");
+              if(expId === "expChoiceDance") {
 
-              setTimeout(function() {
-                let mouseAnimatedDance = document.querySelector("#mouse-animated-dance");
-                mouseAnimatedDance.classList.remove('fadeOut');
-                mouseAnimatedDance.classList.add('fadeInUp');
-                isExperience = false;
-              }, 10000);
+                $("#dance-first-slide").css("background-color", "white");
+                $("#img-dancers").css("opacity", "1");
 
-              $('#dance-section').on('mousewheel', function(e, delta) {
-                if(!isExperience) {
-                  $("#img-dancers-2").css("opacity", "1");
+                setTimeout(function() {
                   let mouseAnimatedDance = document.querySelector("#mouse-animated-dance");
-                  mouseAnimatedDance.classList.remove('fadeInUp');
-                  mouseAnimatedDance.classList.add('fadeOut');
-                  $("#img-dancers-3").css("opacity", "1");
-                  mouseAnimatedDance.addEventListener('animationend', function() { endAnimationDance = true; });
-                  if(endAnimationDance) {
-                    this.scrollLeft -= (delta * 10);
+                  mouseAnimatedDance.classList.remove('fadeOut');
+                  mouseAnimatedDance.classList.add('fadeInUp');
+                  isExperience = false;
+                }, 10000);
+
+                $('#dance-section').on('mousewheel', function(e, delta) {
+                  if(!isExperience) {
+                    $("#img-dancers-2").css("opacity", "1");
+                    let mouseAnimatedDance = document.querySelector("#mouse-animated-dance");
+                    mouseAnimatedDance.classList.remove('fadeInUp');
+                    mouseAnimatedDance.classList.add('fadeOut');
+                    $("#img-dancers-3").css("opacity", "1");
+                    mouseAnimatedDance.addEventListener('animationend', function() { endAnimationDance = true; });
+                    if(endAnimationDance) {
+                      this.scrollLeft -= (delta * 10);
+                    }
                   }
-                }
-                //e.preventDefault();
-              });
+                  //e.preventDefault();
+                });
 
-            } else if (expId === "expChoiceExpo") {
+              } else if (expId === "expChoiceExpo") {
 
-              initArticle('expo');
+                initArticle('expo');
 
-            }
+              }
 
-          });
-          countScroll++;
-        }, 500);
-      }
+            });
+            countScroll++;
+          }, 500);
+        }
 
+      });
     });
+  }
 
+  choicesId.forEach(choiceId => {
+    choiceClick(choiceId);
   });
 
   let menuPos = $("#menu-section").position();
