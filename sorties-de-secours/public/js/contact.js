6a07b16b288bf8714a59d1bad79f7b0312ed325c
Gabrielle de Massol <gabrielle.de-massol-soil@etu.univ-nantes.fr>
2020-02-10 22:14:38 +0100

    gabrielle
    rectification contact

---
+++ sorties-de-secours/public/js/contact.js
@@ -0,0 +1,7 @@
+$('#btnSend').click(function (event) {
+        var email = 'cestparla@sortiesdesecours.com';
+        var subject = 'Ici votre sujet ';
+        var emailBody = 'Ici votre message (Pour la planète pensez à vider votre boîte mail régulièrement ;))';
+        var attach = 'path';
+        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+"?attach="+attach;
+    });
