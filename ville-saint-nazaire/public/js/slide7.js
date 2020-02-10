711291aac9c4d5ea61d7814ac13e3a331ce292e0
Julien Le Roux <julien.le-roux@etu.univ-nantes.fr>
2020-02-10 21:39:32 +0100

    [modif choix 2]

--- ville-saint-nazaire/public/js/slide7.js
+++ ville-saint-nazaire/public/js/slide7.js
@@ -1,8 +1,47 @@
 let initSlide7 = function(){
 
-  d3.select('#next9-2').on('click', function(){
+
+	// Animations bouton suivant
+	d3.select('#slide7-suivant').on('mouseover', function(){
+        anime({
+            targets: '#slide7-btnSuivant',
+            scale: 1.1
+        });
+    });
+
+    d3.select('#slide7-suivant').on('mouseout', function(){
+        anime({
+            targets:  '#slide7-btnSuivant',
+            scale: 1
+        });
+    });
+
+
+    // Animations bouton retour
+	d3.select('#slide7-btnRetour').on('mouseover', function(){
+        anime({
+            targets: '#slide7-btnRetour',
+            scale: 1.1
+        });
+    });
+
+    d3.select('#slide7-btnRetour').on('mouseout', function(){
+        anime({
+            targets:  '#slide7-btnRetour',            
+            scale: 1
+        });
+    });
+
+  d3.select('#slide7-suivant').on('click', function(){
     moveDown();
     initSlide9();
   });
 
+  d3.select('#slide7-btnRetour').on('click', function(){
+    moveUp();
+    moveLeft();
+    //initSlide9();
+  });
 }
+
+
