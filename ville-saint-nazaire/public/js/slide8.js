a9d89d2ab17039faf79f15144c7086f264dd606d
Alan Le Verge <alan.le-verge@etu.univ-nantes.fr>
2020-02-10 22:19:36 +0100

    ajout animation choix 3 + correction slide 4

--- ville-saint-nazaire/public/js/slide8.js
+++ ville-saint-nazaire/public/js/slide8.js
@@ -1,5 +1,61 @@
 let initSlide8 = function(){
 
+
+    function myMove(id,top1,left1) {
+
+    var elem = document.getElementById(id);
+
+    var top = 0;
+    var left = 50;
+
+    let coeff1 = (top1)/8;
+    let coeff2 = (left1-50)/8;
+
+    if(left1<=50){
+      var id = setInterval(frame, 100);
+
+      function frame() {
+
+        if (top >= top1) {
+          clearInterval(id);
+          console.log('1');
+        } else {
+            console.log('2');
+          top=top+coeff1;
+          left=left+coeff2;
+          elem.style.top = top + '%';
+          elem.style.left = left + '%';
+        }
+      }
+    }else if(left1>50){
+      var id = setInterval(frame, 100);
+
+      function frame() {
+
+        if (top >= top1 || left >= 74) {
+            console.log('3');
+          clearInterval(id);
+        } else {
+            console.log('4');
+            //console.log(top);
+          top=top+coeff1;
+          left=left+coeff2;
+          elem.style.top = top + '%';
+          elem.style.left = left + '%';
+        }
+      }
+
+
+    }
+  }
+
+
+    myMove("slide8-img1", 45, 3);
+    myMove("slide8-img2", 15, 11.5);
+    myMove("slide8-img3", 9, 39);
+    myMove("slide8-img4", 30.5, 63);
+    myMove("slide8-img5", 0.01, 74);
+
 	// Animations bouton suivant
 	d3.select('#slide8-suivant').on('mouseover', function(){
         anime({
