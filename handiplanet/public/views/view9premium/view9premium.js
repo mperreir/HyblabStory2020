11fe025e51f4af74d1618324947be5a6f2fb54a1
Eric Bizet <eric.bizet@etu.univ-nantes.fr>
2020-02-10 17:11:37 +0100

    View9premium: bulles

--- handiplanet/public/views/view9premium/view9premium.js
+++ handiplanet/public/views/view9premium/view9premium.js
@@ -16,6 +16,20 @@ class View9Premium extends View {
     this.nextButton.addEventListener('click', () => {
       this.switchToView10();
     });
+
+    this.bulle1 = document.getElementById('view9p-bulle1');
+    this.bulle2 = document.getElementById('view9p-bulle2');
+    this.bulle3 = document.getElementById('view9p-bulle3');
+    this.bulle4 = document.getElementById('view9p-bulle4');
+
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX - window.innerWidth / 2;
+        const y = e.clientY - window.innerHeight / 2;
+        this.bulle1.style.transform = `translateX(${x * -0.1}px) translateY(${y * -0.05}px)`;
+        this.bulle2.style.transform = `translateX(${x * -0.05}px) translateY(${y * -0.1}px)`;
+        this.bulle3.style.transform = `translateX(${x * 0.08}px) translateY(${y * 0.05}px)`;
+        this.bulle4.style.transform = `translateX(${x * 0.05}px) translateY(${y * -0.1}px)`;
+    });
   }
 
   async switchToView10() {
