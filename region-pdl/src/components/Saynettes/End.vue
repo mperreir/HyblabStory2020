773be4b57a6dc6f16c8bbb990e4ab83e07d60143
Xavier <xavier.tremillon.pro@gmail.com>
2020-02-11 17:25:55 +0100

    added ending logos

--- region-pdl/src/components/Saynettes/End.vue
+++ region-pdl/src/components/Saynettes/End.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="saynette">
     <div class="logo-div">
-      <LogoPolytech class="logo" />
-      <img src="@/assets/End/logo-audencia.png" class="logo" />
-      <img src="@/assets/End/logo-agr.png" class="logo agr" />
       <img src="@/assets/End/logo-hyblab.png" class="logo" />
       <img src="@/assets/End/logo-Nantes.png" class="logo" />
       <img src="@/assets/End/logo-OML.png" class="logo" />
+      <img src="@/assets/End/logo_cc.png" class="logo logo-cc" />
+      <img src="@/assets/End/logo_opensource.png" class="logo logo-cc" />
     </div>
 
     <TextTitle :x="40" :y="15">Félicitations !</TextTitle>
     <Background class="svg" />
 
+    <LogoAgr class="logo-agr" />
     <SimpleText :y="22" :style="{textAlign: 'center'}">
       <p class="text">
         Désormais, vous en savez davantage sur les actions de la Région des Pays de la Loire pour la transition énergétique.
@@ -24,14 +24,16 @@
       </p>
     </SimpleText>
 
-    <SimpleText :y="50" :x="10" :width="20" :style="{textAlign: 'center'}">
-      <span class="large-text">Storytelling<br></span>
+    <img src="@/assets/End/logo-audencia.png" class="logo-audencia" />
+    <SimpleText :y="60" :x="10" :width="20" :style="{textAlign: 'center'}">
+      <span class="large-text">Storytelling</span>
       <br />Nina Naulleau
       <br />Vinciane Le Borgne
     </SimpleText>
 
-    <SimpleText :y="50" :x="35" :width="30" :style="{textAlign: 'center'}">
-      <span class="large-text">Développement et intégration<br></span>
+    <LogoPolytech class="logo-polytech" />
+    <SimpleText :y="60" :x="35" :width="30" :style="{textAlign: 'center'}">
+      <span class="large-text">Développement et intégration</span>
       <br />Julien Garcia
       <br />Nicolas Vautier
       <br />Mathis Le Berrigaud
@@ -39,8 +41,8 @@
       <br />Nathan Seva
     </SimpleText>
 
-    <SimpleText :y="50" :x="70" :width="20" :style="{textAlign: 'center'}">
-      <span class="large-text">Direction artistique<br></span>
+    <SimpleText :y="60" :x="70" :width="20" :style="{textAlign: 'center'}">
+      <span class="large-text">Direction artistique</span>
       <br />Mathilde Mousset
       <br />Marine Boismain
     </SimpleText>
@@ -52,6 +54,7 @@ import Background from "@/assets/End/background-end.svg";
 import TextTitle from "@/components/TextTitle";
 import SimpleText from "@/components/SimpleText";
 import LogoPolytech from "@/assets/End/logo-polytech.svg";
+import LogoAgr from "@/assets/End/logo-agr.svg";
 
 export default {
   name: "Persos",
@@ -59,16 +62,38 @@ export default {
     Background,
     TextTitle,
     SimpleText,
-    LogoPolytech
+    LogoPolytech,
+    LogoAgr
   }
 };
 </script>
 
 <style scoped>
 
+.logo-agr {
+  position: absolute;
+  width: 12%;
+  left: 74%;
+  top: 43%;
+}
+
+.logo-polytech {
+  position: absolute;
+  width: 18%;
+  left: 41%;
+  top: 46%;
+}
+
+.logo-audencia {
+  position: absolute;
+  width: 18%;
+  left: 10%;
+  top: 49%;
+}
+
 .large-text {
   font-weight: bold;
-  font-size: 3vh;
+  font-size: 2vh;
 }
 
 a {
@@ -81,17 +106,21 @@ a:hover {
 
 .logo-div {
   position: absolute;
-  top: 84%;
+  top: 88%;
   left: 2%;
   width: 96%;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 }
 
 .logo {
   width: 15%;
+}
+
+.logo-cc {
+  width: 7%;
 }
 
 .agr {
