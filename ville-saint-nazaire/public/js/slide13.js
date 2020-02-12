f45cc794888bf7ee823777d3dbca9b8f8454df60
simonblgr <s.bellanger1@gmail.com>
2020-02-12 11:53:32 +0100

    corrections demandées par commanditaire

--- ville-saint-nazaire/public/js/slide13.js
+++ ville-saint-nazaire/public/js/slide13.js
@@ -1,30 +1,11 @@
 let initSlide13 = function(){
 
-  let pause = 0;
-  var vid = document.getElementById("videofin");
-  var play = document.getElementById("play");
-
   d3.select('#next14-img').on('click', function(){
     vid.pause();
     moveRight();
     initSlide14();
   });
 
-  d3.select('#play').on('click', function(){
-    vid.play();
-    play.style.visibility='hidden';
-  });
-
-  d3.select('#videofin').on('click', function(){
-    if(pause == 0){
-      vid.pause();
-      pause=1;
-    } else{
-      vid.play();
-      pause=0;
-    }
-
-  });
 
   d3.select('#next14-img').on('mouseover', function(){
     anime({
